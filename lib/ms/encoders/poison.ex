e7c07257f16187fecfd4ee0afee526f849754501
Christian Froehlingsdorf <christian.froehlingsdorf@rewe-digital.com>
2017-05-13 16:44:31 +0200

    comment for encoder

--- lib/ms/encoders/poison.ex
+++ lib/ms/encoders/poison.ex
@@ -1,4 +1,8 @@
 defimpl Poison.Encoder, for: Any do
+    # this encoder is required when trying to json stringify
+    # ecto query results, as they may contain private data
+    # that will throw exceptions per default
+
   def encode(%{__struct__: _} = struct, options) do
     map = struct
           |> Map.from_struct
