4ae300971460a835a0f44db7a03922afe6460c8c
Holger Adam <holger.adam@rewe-digital.com>
2017-06-11 12:49:59 +0200

    moves cassandra to its own GenServer

--- lib/data/cassandra.ex
+++ lib/data/cassandra.ex
@@ -1,32 +1,55 @@
 defmodule ExTest.Cassandra do
-
+  use GenServer
   alias MSBase.Registry
 
-  # TODO wrap in a GenServer
+  @process_key "cassandra-genserver"
 
-  def init do
+  def start(_) do
+    {:ok, pid} = GenServer.start_link(__MODULE__, nil)
+    Registry.set(@process_key, pid)
+  end
+
+  def init() do
     {:ok, conn} = Xandra.start_link(
         host: Application.get_env(:ex_test, :cassandra_host),
         port: Application.get_env(:ex_test, :cassandra_port)
     )
 
-    Registry.set("cassandra-conn", conn)
+    Registry.set(@process_key, conn)
     {:ok, conn}
   end
 
   def void_query(statement, params) do
-    {:ok, conn} = Registry.get "cassandra-conn"
-    {:ok, %Xandra.Void{}} = Xandra.execute(conn, statement, params)
+    {:ok, pid} = Registry.get(@process_key)
+    GenServer.cast(pid, {:void_query, statement, params})
   end
 
   def list_query(statement, params) do
-    {:ok, conn} = Registry.get "cassandra-conn"
-    {:ok, %Xandra.Page{}} = Xandra.execute(conn, statement, params)
+    {:ok, pid} = Registry.get(@process_key)
+    GenServer.call(pid, {:list_query, statement, params})
   end
 
   def prepare(statement) do
+    {:ok, pid} = Registry.get(@process_key)
+    GenServer.call(pid, {:prepare, statement})
+  end
+
+  def handle_cast({:void_query, statement, params}, state) do
+    {:ok, conn} = Registry.get "cassandra-conn"
+    {:ok, %Xandra.Void{}} = Xandra.execute(conn, statement, params)
+    {:noreply, state}
+  end
+
+  def handle_call({:list_query, statement, params}, _, state) do
+    {:ok, conn} = Registry.get "cassandra-conn"
+    {:ok, %Xandra.Page{} = page} = Xandra.execute(conn, statement, params)
+    {:reply, page, state}
+  end
+
+  def handle_call({:prepare, statement}, _, state) do
     {:ok, conn} = Registry.get "cassandra-conn"
     {:ok, prepared} = Xandra.prepare(conn, statement)
+    {:reply, prepared, state}
   end
 
 end
